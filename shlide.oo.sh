fbcc5e29c32d5e3dd76249064080bd797108c636
Cj-bc <cj-bc@y-modify.org>
2018-11-04 19:45:38 +0900

    Remove: slide class

--- shlide.oo.sh
+++ shlide.oo.sh
@@ -9,37 +9,6 @@
 source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
 import util/class util/log util/exception util/tryCatch
 
-class:Slide() {
-  private string path
-
-  # initialize slide
-  # @param <string slide_path>
-  Slide.__init__() {
-    [string] slide_path
-
-    [[ ! -f "$slide_path" ]] && e="Not found" throw && return
-    this path = "$slide_path"
-    @return
-  }
-
-  # print slide
-  Slide.print() {
-
-    local path="$(this path)"
-    [ -z "$path" ] && e="path not defined" throw && return
-    tput clear
-    tput cup 0 0
-    cat "$path"
-    @return
-  }
-
-  # echo all methods of this class
-  Slide::__dir__() {
-    compgen -c | grep 'Slide[.:]' | sed 's/Slide[.:]*//g'
-    @return
-  }
-}
-
 class:Shlide() {
   private string root_path
   private integer counter
@@ -80,5 +49,4 @@ class:Shlide() {
 
 }
 
-Type::Initialize Slide
 Type::InitializeStatic Shlide
